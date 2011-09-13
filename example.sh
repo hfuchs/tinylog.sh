caa26d183ddfc6766b3c47845f326d6f4d5a23a4
Hagen Fuchs <hagen.fuchs@physik.tu-dresden.de>
2011-09-13 19:38:20 +0200

    example.sh: Flesh out for new tinylog.sh.

--- example.sh
+++ example.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+# 2011-09-13, Created by H Fuchs <github@hfuchs.net>
 
+logfile=my.log
 source ./tinylog.sh
 
 debug "A debug statement."
 error "An error statement."
 warn "A warning statement."
+echo "A normal command output (only visible in the log)."
+ls xdlfkj  # Will print error message to stderr
 
+debug "Log file is '$logfile'."
 
