400749ec5af3021b4d26b986958f04a79d3e3139
H Fuchs <hfuchs@pks.mpg.de>
2011-10-11 13:02:54 +0200

    tinylog.sh: Handle logfile safer and more intuitively (create temp-file by default).

--- tinylog.sh
+++ tinylog.sh
@@ -1,26 +1,36 @@
 # 2011-09-13, Created by H Fuchs <github@hfuchs.net>
 # GPLv3-licensed, see COPYING.  Official disclaimer in DISCLAIMER.
 
-# TODO Introduce do_syslog variable?
+# TODO Introduce tinylog_do_syslog variable?
 # TODO Introduce, for that matter, the concept of output streams?
 # TODO Test for availability of logger and tty.
+# TODO Get rid of bashisms ('source' -> '.')!
 
 function tinylog_init {
     # Is the user watching us perform?  Better produce some output, then!
     tty -s && tinylog_interactive=1
 
+    # Use mktemp (and parameter substitution) if $logfile is not specified.
+    test -z "$logfile" && logfile=`mktemp --tmpdir $(basename $0).XXXXX.log`
+
     # Set up IO redirection if a logfile got specified.
     stdout="/dev/stdout"
     stderr="/dev/stderr"
-    if [ -n "$logfile" ]; then
+    if (echo -n "" >> "$logfile") 2> /dev/null; then
         exec 3<&1
         exec >> $logfile               # Append, append!
         exec 4<&2
         exec 2>> $logfile
         stdout="/dev/fd/3"
-        stderr="/dev/fd/4"             # TODO Useful?
+        stderr="/dev/fd/4"
         tinylog_redirect=1
-        echo "--- tinylog start (`date +%s`) ---"
+        echo "--- tinylog start: `date` (`date +%s`) ---"
+    else
+        echo "tinylog: Logfile '$logfile' is not writeable!" > $stderr
+        echo "tinylog: Script will run - just without superior logging." > $stderr
+        trap "" INT TERM EXIT
+        stdout="/dev/null"  # Suppress duplicate messages, then.
+        stderr="/dev/null"
     fi
 }
 
