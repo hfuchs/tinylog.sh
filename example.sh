20baa5b765e76d04d260c735cdd669d40b686a9d
H Fuchs <hfuchs@pks.mpg.de>
2011-10-11 13:02:57 +0200

    example.sh: Show off new features (default logfile, printlog).

--- example.sh
+++ example.sh
@@ -2,14 +2,21 @@
 # 2011-09-13, Created by H Fuchs <github@hfuchs.net>
 # GPLv3-licensed, see COPYING.  Official disclaimer in DISCLAIMER.
 
-logfile=my.log
-source ./tinylog.sh
+#logfile=`dirname $0`/my.log     # If you want continuous logs.
+#logfile="/etc/passwd"           # Witness how tinylog copes with that!  Ha!
+source `dirname $0`/tinylog.sh   # If tinylog.sh is in the same directory.
+#source tinylog.sh               # If it's elsewhere and elsewhere is in $PATH.
 
 debug "A debug statement."
 error "An error statement."
 warn "A warning statement."
 echo "A normal command output (only visible in the log)."
-ls xdlfkj  # Will print error message to stderr
 
-debug "Log file is '$logfile'."
+ls non_existent  # Will print error message to stderr (unless... :).
+
+debug "Log file is '$logfile'."  # Always a good idea.
+debug "For even more fun, take a look inside '$0'."
+
+#printlog                          # Useful for cronjobs perhaps.
+#printlog "Optional introductory error message."
 
