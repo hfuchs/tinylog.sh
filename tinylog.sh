61ab4317fcafc84041db2a30ebe55d011520c79c
Hagen Fuchs <code@hfuchs.net>
2012-03-14 10:56:47 +0100

    tinylog.sh: Streamline output redirection; add test for 'logger'.

--- tinylog.sh
+++ tinylog.sh
@@ -1,69 +1,68 @@
-# 2011-09-13, Created by H Fuchs <code@hfuchs.net>
+# 2011-09-13, Created by Hagen Fuchs <code@hfuchs.net>
 # GPLv3-licensed, see COPYING.  Official disclaimer in DISCLAIMER.
 
-# TODO Introduce tinylog_do_syslog variable?
-# TODO Introduce, for that matter, the concept of output streams?
-# TODO Test for availability of logger and tty.
-# TODO Get rid of bashisms ('source' -> '.')!
+# TODO Introduce the concept of output streams?
 
 tinylog_init () {
     # Is the user watching us perform?  Better produce some output, then!
     tty -s && tinylog_interactive=1
 
+    # May we log?
+    test -x `which logger` && tinylog_syslog=1
+
     # Use mktemp (and parameter substitution) if $logfile is not specified.
     test -z "$logfile" && logfile=$(mktemp -t $(basename $0).log.XXXXX)
 
+    stdout="1"
+    stderr="2"
     # Set up IO redirection if a logfile got specified.
-    stdout="/dev/stdout"
-    stderr="/dev/stderr"
+    # See section REDIRECTION in bash(1).
     if (echo -n "" >> "$logfile") 2> /dev/null; then
-        exec 3<&1
+        exec 3>&1               # Duplicate stdout to fd 3, keep 1 open
+        exec 4>&2               # Duplicate stderr to fd 4
         exec >> $logfile               # Append, append!
-        exec 4<&2
         exec 2>> $logfile
-        stdout="/dev/fd/3"
-        stderr="/dev/fd/4"
+        stdout="3"
+        stderr="4"
         tinylog_redirect=1
         echo "--- tinylog start: $(date) ($(date +%s)) ---"
     else
-        echo "tinylog: Logfile '$logfile' is not writeable!" > $stderr
-        echo "tinylog: Script will run - just without superior logging." > $stderr
+        echo "tinylog: Logfile '$logfile' is not writeable!" >&$stderr
+        echo "tinylog: Script will run - just without superior logging." >&$stderr
         trap "" INT TERM EXIT
-        stdout="/dev/null"  # Suppress duplicate messages, then.
-        stderr="/dev/null"
     fi
 }
 
 tinylog_exit () {
-    # Restore normal IO flow (Bash idiom, see §20.1 in The Advanced
-    # Bash-Scripting Guide).
-    [ $tinylog_redirect ] && exec 1>&3 3>&-
-    [ $tinylog_redirect ] && exec 2>&4 4>&-
+    # Restore normal IO flow (Bash idiom, see
+    # http://tldp.org/LDP/abs/html/io-redirection.html)
+    [ $tinylog_redirect ] && exec 1>&3 3>&-  # reinstate stdout
+    [ $tinylog_redirect ] && exec 2>&4 4>&-  # reinstate stdin
 }
 
 debug () {
     [ $tinylog_interactive ] || return 0
     #echo "[DEBUG] $@"  # 2011-10-07, Don't debug() into my log file!
-    echo "[DEBUG] $@" > $stdout
+    echo "[DEBUG] $@" >&$stdout
 }
 
 warn () {
-    logger -t $(basename $0) -p user.warning "[WARN] $@"
     echo "[WARN] $@"
-    echo "[WARN] $@" > $stderr
+    [ $tinylog_syslog ]   && logger -t $(basename $0) -p user.warning "[WARN] $@"
+    [ $tinylog_redirect ] && echo "[WARN] $@" >&$stderr
 }
 
 error () {
-    logger -t $(basename $0) -p user.err "[ERROR] $@"
     echo "[ERROR] $@"
-    echo "[ERROR] $@" > $stderr
+    [ $tinylog_syslog ]   && logger -t $(basename $0) -p user.err "[ERROR] $@"
+    [ $tinylog_redirect ] && echo "[ERROR] $@" >&$stderr
 }
 
 printlog () {
     msg=$1
-    [ -n "$msg" ] && echo $msg > $stdout  # TODO Not at all sure about that.
-    echo "--------- Log Contents ($logfile) ---------" > $stdout
-    cat $logfile > $stdout
+    [ -n "$msg" ] && echo "$msg\n" >&$stdout
+    echo "--------- Log Contents ($logfile) ---------" >&$stdout
+    cat $logfile >&$stdout
 }
 
 #------------------------------ main ------------------------------
